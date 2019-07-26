3c452a11e15e0440be867a4766abd83e8156ec15
Redo <redar_chicho@hotmail.com>
2019-07-25 21:59:05 -0500

    code added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
 
+  state={
+    post:{}
+  }
+
+
+   componentDidMount(){
+        axios.get("https://jsonplaceholder.typicode.com/posts").then((data)=>{
+          console.log(data);
+          this.setState({post:data});
+        })
+   }
   render(){
+    console.log(this.state.post)
+
     return (
       <div className="App">
        <h1>Hi I am React</h1>
